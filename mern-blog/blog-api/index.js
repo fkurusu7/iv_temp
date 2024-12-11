e3f63498c12067712705402782bf2e9ce98fef09
fkurusu7 <coding.fcv@gmail.com>
2024-12-11 13:35:59 -0600

    connect to db

--- mern-blog/blog-api/index.js
+++ mern-blog/blog-api/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(console.log("Database connected"))
+  .catch((error) => console.log(error));
 
 const app = express();
 
