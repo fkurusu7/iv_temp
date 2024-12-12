7aac93d0131f3f3116df37d26873832bd2d9bd75
fkurusu7 <coding.fcv@gmail.com>
2024-12-11 19:53:18 -0600

    add middleware to handle errors

--- mern-blog/blog-api/index.js
+++ mern-blog/blog-api/index.js
@@ -17,6 +17,13 @@ app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
+// handles errors
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error - Index";
+  res.status(statusCode).json({ success: false, statusCode, message });
+});
+
 app.listen(5174, () => {
   console.log("Server running on port 5174!!");
 });
--- mern-blog/blog-api/src/controllers/auth.controller.js
+++ mern-blog/blog-api/src/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHAndler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
-    return res.status(400).json({ message: "All fields are required" });
+    next(errorHAndler(400, "All fields are required"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -20,8 +21,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     return res.status(201).json({ message: "Signed up successfully!" });
   } catch (error) {
-    return res
-      .status(400)
-      .json({ message: "Server Error", error: error.message });
+    next(error);
   }
 };
