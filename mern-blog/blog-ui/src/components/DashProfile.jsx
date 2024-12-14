89d42cb28e2ce0a58eeb30d51372d707ce543a01
fkurusu7 <coding.fcv@gmail.com>
2024-12-13 21:22:24 -0600

    connect ui to api user update functionality

--- mern-blog/blog-ui/src/components/DashProfile.jsx
+++ mern-blog/blog-ui/src/components/DashProfile.jsx
@@ -1,12 +1,18 @@
-import { Button, TextInput } from "flowbite-react";
+import { Alert, Button, Spinner, TextInput } from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import { updateStart, updateSuccess, updateFailure } from "../redux/userSlice";
+import { useDispatch } from "react-redux";
 
 function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [formData, setFormData] = useState({});
+  const [updatedUserSuccess, setUpdatedUserSuccess] = useState(null);
   const filePickerRef = useRef();
+  const dispatch = useDispatch();
+  const { isLoading, error: errorMessage } = useSelector((state) => state.user);
 
   const handleImageChange = (ev) => {
     const file = ev.target.files[0];
@@ -14,6 +20,10 @@ function DashProfile() {
       setImageFile(file);
       setImageFileUrl(URL.createObjectURL(file));
     }
+  };
+
+  const handleChange = (ev) => {
+    setFormData({ ...formData, [ev.target.id]: ev.target.value });
   };
 
   const uploadImage = async () => {};
@@ -24,10 +34,38 @@ function DashProfile() {
     }
   }, [imageFile]);
 
+  const handleSubmit = async (ev) => {
+    ev.preventDefault();
+
+    if (Object.keys(formData).length === 0) return;
+
+    try {
+      dispatch(updateStart());
+      const res = await fetch(`/api/user/update/${currentUser.user.id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const jsonRes = await res.json();
+
+      console.log(jsonRes);
+      if (!res.ok) {
+        dispatch(updateFailure(jsonRes.message));
+      } else {
+        dispatch(updateSuccess(jsonRes));
+        setUpdatedUserSuccess("User updated successfully!");
+      }
+    } catch (error) {
+      dispatch(updateFailure(error.message));
+    } finally {
+      setUpdatedUserSuccess(null);
+    }
+  };
+
   return (
     <div className="w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="file"
           name="picture"
@@ -52,23 +90,41 @@ function DashProfile() {
           id="name"
           placeholder="username"
           defaultValue={currentUser.user.name}
+          onChange={handleChange}
         />
         <TextInput
           type="email"
           id="email"
           placeholder="email"
           defaultValue={currentUser.user.email}
+          onChange={handleChange}
         />
-        <TextInput type="password" id="password" placeholder="password" />
+        <TextInput
+          type="password"
+          id="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
 
-        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
-          Update
+        <Button
+          type="submit"
+          gradientDuoTone="purpleToBlue"
+          outline
+          disabled={isLoading}
+        >
+          {isLoading ? <Spinner size="sm" /> : "Update"}
         </Button>
       </form>
+      {errorMessage && <div>{errorMessage}</div>}
       <div className="text-red-500 flex justify-between mt-5">
         <span className="cursor-pointer">Delete Account</span>
         <span className="cursor-pointer">Sign put</span>
       </div>
+      {updatedUserSuccess && (
+        <Alert color="success" className="mt-5">
+          {updatedUserSuccess}
+        </Alert>
+      )}
     </div>
   );
 }
--- mern-blog/blog-ui/src/redux/userSlice.js
+++ mern-blog/blog-ui/src/redux/userSlice.js
@@ -23,8 +23,28 @@ const userSlice = createSlice({
       state.isLoading = false;
       state.error = action.payload;
     },
+    updateStart: (state) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.isLoading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const { signinStart, signinSuccess, signinFailure } = userSlice.actions;
+export const {
+  signinStart,
+  signinSuccess,
+  signinFailure,
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} = userSlice.actions;
 export default userSlice.reducer;
