3d347ccc6601adf5d2da974640f6cd7541cb58ba
fkurusu7 <coding.fcv+github@gmail.com>
2025-06-13 15:36:04 -0600

    add multiple pointers exercise

--- javascript/dsa/1.solving_patterns/2.multiple_pointers.js
+++ javascript/dsa/1.solving_patterns/2.multiple_pointers.js
@@ -66,3 +66,81 @@ console.log(countUniqueValues([1, 1, 1, 1, 1, 2])); // ==> 2
 console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // ==> 7
 // console.log(countUniqueValues([])); // ==> 0
 // console.log(countUniqueValues([-2, -1, -1, 0, 1])); // ==> 4
+
+/* averagePair
+  Given a sorted array of integers and a target average, determine if there is a pair
+  of values in the array where the average of the pair equals the target average. 
+  There may be more than one pair that matches the average target.
+
+  Bonus Constraints:
+    Time: O(N)
+    Space: O(1)
+
+  Examples:
+    averagePair([1,2,3],2.5) // true
+    averagePair([1,3,3,5,6,7,10,12,19],8) // true
+    averagePair([-1,0,3,4,5,6], 4.1) // false
+    averagePair([],4) // false
+
+*/
+
+function averagePair(nums, target) {
+  if (nums.length === 0) return false;
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    const calcAver = (nums[left] + nums[right]) / 2;
+
+    if (calcAver === target) {
+      return true;
+    } else if (calcAver > target) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return false;
+}
+
+console.log(averagePair([1, 2, 3], 2.5)); // ==> true
+console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)); // ==> true
+console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1)); // ==> false
+console.log(averagePair([], 4)); // ==> false
+
+/* isSubsequence
+  Given two strings, check whether the characters in the first string form a 
+  subsequence of the characters in the second string. 
+  In other words, the function should check whether the characters in the first 
+  string appear somewhere in the second string, without their order changing.
+
+  Examples:
+    isSubsequence('hello', 'hello world'); // true
+    isSubsequence('sing', 'sting'); // true
+    isSubsequence('abc', 'abracadabra'); // true
+    isSubsequence('abc', 'acb'); // false (order matters)
+  
+  Complexities:
+    Time Complexity - O(N + M)
+    Space Complexity - O(1)
+*/
+
+function isSubsequence(subSeq, sequence) {
+  let subIndex = 0;
+  let seqIndex = 0;
+
+  while (seqIndex <= sequence.length) {
+    if (subSeq[subIndex] === sequence[seqIndex]) subIndex++;
+    if (subIndex === subSeq.length) return true;
+    seqIndex++;
+  }
+
+  return false;
+}
+
+console.log(isSubsequence('hello', 'hello world')); // ==> true
+console.log(isSubsequence('sing', 'sting')); // ==> true
+console.log(isSubsequence('abc', 'abracadabra')); // ==> true
+console.log(isSubsequence('abc', 'acb')); // ==> false (order matters)
