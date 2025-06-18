309896395e37118ff5a45a12fa6288850e749c33
fkurusu7 <coding.fcv+github@gmail.com>
2025-06-17 22:03:08 -0600

    add new exercise for multiple pointers pattern

--- javascript/dsa/1.solving_patterns/2.multiple_pointers.js
+++ javascript/dsa/1.solving_patterns/2.multiple_pointers.js
@@ -144,3 +144,62 @@ console.log(isSubsequence('hello', 'hello world')); // ==> true
 console.log(isSubsequence('sing', 'sting')); // ==> true
 console.log(isSubsequence('abc', 'abracadabra')); // ==> true
 console.log(isSubsequence('abc', 'acb')); // ==> false (order matters)
+
+/* findPair
+  Given an unsorted array and a number n, find if there exists a pair of elements 
+  in the array whose difference is n. 
+  This function should return true if the pair exists or false if it does not.
+
+  Examples:
+    findPair([6,1,4,10,2,4], 2) // true
+    findPair([8,6,2,4,1,0,2,5,13],1) // true
+    findPair([4,-2,3,10],-6) // true
+    findPair([6,1,4,10,2,4], 22) // false
+    findPair([], 0) // false
+    findPair([5,5], 0) // true
+    findPair([-4,4], -8) // true
+    findPair([-4,4], 8) // true
+    findPair([1,3,4,6],-2) // true
+    findPair([0,1,3,4,6],-2) // true
+    findPair([1,2,3], 0) // false
+  
+  Part 1 - solve this with the following requirements:
+    - Time Complexity Requirement - O(n)
+    - Space Complexity Requirement - O(n)
+
+  Part 2 - solve this with the following requirements:
+    - Time Complexity Requirement - O(n log n)
+    - Space Complexity Requirement - O(1)
+*/
+
+function findPair(arr, target) {
+  if (!arr.length) return false;
+
+  arr.sort((a, b) => a - b);
+  let i = 0;
+  let j = 1;
+
+  while (i < arr.length && j < arr.length) {
+    if (i !== j && Math.abs(arr[i] - arr[j]) === Math.abs(target)) {
+      return true;
+    } else if (arr[j] - arr[i] < target) {
+      j++;
+    } else {
+      i++;
+    }
+  }
+
+  return false;
+}
+
+console.log(findPair([6, 1, 4, 10, 2, 4], 2)); // ==> true
+console.log(findPair([8, 6, 2, 4, 1, 0, 2, 5, 13], 1)); // ==> true
+console.log(findPair([4, -2, 3, 10], -6)); // ==> true
+console.log(findPair([6, 1, 4, 10, 2, 4], 22)); // ==> false
+console.log(findPair([], 0)); // ==> false
+console.log(findPair([5, 5], 0)); // ==> true
+console.log(findPair([-4, 4], -8)); // ==> true
+console.log(findPair([-4, 4], 8)); // ==> true
+console.log(findPair([1, 3, 4, 6], -2)); // ==> true
+console.log(findPair([0, 1, 3, 4, 6], -2)); // ==> true
+console.log(findPair([1, 2, 3], 0)); // ==> false
